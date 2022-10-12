d7a6da298baf688bc4464c4a0e7412e83e7de4e6
luisrivas123 <luis.rivas@correounivalle.edu.co>
2022-10-12 16:28:36 -0500

    client mode receive listening

--- client/client.go
+++ client/client.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"bufio"
+	"io"
 	"flag"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
+	"path/filepath"
 )
+
+const BUFFERSIZE = 1024
 
 type ClientManager struct {
 	clients    map[*Client]bool
@@ -49,17 +54,54 @@ func sendClientMode() {
 }
 
 func receiveClientMode() {
-	fmt.Println("Starting client recieve...")
-	connection, error := net.Dial("tcp", "localhost:3000")
-	if error != nil {
-		fmt.Println(error)
-	}
-	client := &Client{socket: connection}
-	go client.receive()
+	// fmt.Println("Starting client recieve...")
+	// connection, error := net.Dial("tcp", "localhost:3000")
+	// if error != nil {
+	// 	fmt.Println(error)
+	// }
+	// client := &Client{socket: connection}
+	// go client.receive()
+	go receiveFile()
 
 	var input string
 	fmt.Scanln(&input)
 
+}
+
+func receiveFile() {
+	connection, err := net.Dial("tcp", "localhost:3000")
+	if err != nil {
+		panic(err)
+	}
+	defer connection.Close()
+	fmt.Println("Connected to server, start receiving the file name and file size")
+	bufferFileName := make([]byte, 64)
+	bufferFileSize := make([]byte, 10)
+
+	connection.Read(bufferFileSize)
+	fileSize, _ := strconv.ParseInt(strings.Trim(string(bufferFileSize), ":"), 10, 64)
+
+	connection.Read(bufferFileName)
+	fileName := strings.Trim(string(bufferFileName), ":")
+	path := filepath.Join("./file/", fileName)
+	newFile, err := os.Create(path)
+
+	if err != nil {
+		panic(err)
+	}
+	defer newFile.Close()
+	var receivedBytes int64
+
+	for {
+		if (fileSize - receivedBytes) < BUFFERSIZE {
+			io.CopyN(newFile, connection, (fileSize - receivedBytes))
+			connection.Read(make([]byte, (receivedBytes+BUFFERSIZE)-fileSize))
+			break
+		}
+		io.CopyN(newFile, connection, BUFFERSIZE)
+		receivedBytes += BUFFERSIZE
+	}
+	fmt.Println("Received file completely!")
 }
 
 func main() {
