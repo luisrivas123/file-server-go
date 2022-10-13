2db0f9e51399948c47f6751e1f455d36f5f50efa
luisrivas123 <luis.rivas@correounivalle.edu.co>
2022-10-12 19:47:17 -0500

    server sending file client independent

--- server/server.go
+++ server/server.go
@@ -2,31 +2,24 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
+	"io"
 	"os"
 	"strconv"
 )
 
 const BUFFERSIZE = 1024
 
-func main() {
-	server, err := net.Listen("tcp", "localhost:3000")
-	if err != nil {
-		fmt.Println("Error listetning: ", err)
-		os.Exit(1)
-	}
-	defer server.Close()
-	fmt.Println("Server started! Waiting for connections...")
-	for {
-		connection, err := server.Accept()
-		if err != nil {
-			fmt.Println("Error: ", err)
-			os.Exit(1)
-		}
-		fmt.Println("Client connected")
-		go sendFileToClient(connection)
-	}
+type ClientManager struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+type Client struct {
+	socket net.Conn
+	data   chan []byte
 }
 
 func sendFileToClient(connection net.Conn) {
@@ -70,4 +63,90 @@ func fillString(retunString string, toLength int) string {
 		break
 	}
 	return retunString
+}
+
+func (manager *ClientManager) start() {
+	for {
+		select {
+		case connection := <-manager.register:
+			manager.clients[connection] = true
+			fmt.Println("Added new connection!")
+		case connection := <-manager.unregister:
+			if _, ok := manager.clients[connection]; ok {
+				close(connection.data)
+				delete(manager.clients, connection)
+				fmt.Println("A connection has terminated!")
+			}
+		case message := <-manager.broadcast:
+			for connection := range manager.clients {
+				select {
+				case connection.data <- message:
+				default:
+					close(connection.data)
+					delete(manager.clients, connection)
+				}
+			}
+		}
+	}
+}
+
+func (manager *ClientManager) receive(client *Client) {
+	for {
+		message := make([]byte, 4096)
+		length, err := client.socket.Read(message)
+		if err != nil {
+			manager.unregister <- client
+			client.socket.Close()
+			break
+		}
+		if length > 0 {
+			fmt.Println("RECEIVED 1: " + string(message))
+			manager.broadcast <- message
+		}
+	}
+}
+
+func startServerMode() {
+	fmt.Println("Starting server...")
+	listener, error := net.Listen("tcp", ":3000")
+	if error != nil {
+		fmt.Println(error)
+	}
+	manager := ClientManager{
+		clients:    make(map[*Client]bool),
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+	}
+	go manager.start()
+	for {
+		connection, _ := listener.Accept()
+		if error != nil {
+			fmt.Println(error)
+		}
+		client := &Client{socket: connection, data: make(chan []byte)}
+		manager.register <- client
+		go manager.receive(client)
+		go manager.send(client)
+		//
+		fmt.Println("Client connected")
+		go sendFileToClient(connection)
+	}
+}
+
+func (manager *ClientManager) send(client *Client) {
+	defer client.socket.Close()
+	for {
+		select {
+		case message, ok := <-client.data:
+			if !ok {
+				return
+			}
+			client.socket.Write(message)
+		}
+	}
+}
+
+func main() {
+	startServerMode()
 }
