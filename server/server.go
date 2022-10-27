54d11abc7b7b35eacb21c3782ebdfab8b9c6afe2
luisrivas123 <luis.rivas@correounivalle.edu.co>
2022-10-26 21:49:02 -0500

    counter of clients connected to the server

--- server/server.go
+++ server/server.go
@@ -12,6 +12,10 @@ import (
 
 const BUFFERSIZE = 1024
 
+var countClientsChannel1 = 0
+var countClientsChannel2 = 0
+var countClientsChannel3 = 0
+
 type ClientManager struct {
 	clients    map[*Client]bool
 	broadcast  chan []byte
@@ -70,17 +74,79 @@ func fillString(retunString string, toLength int) string {
 	return retunString
 }
 
-func (manager *ClientManager) start() {
+func (manager *ClientManager) start1() {
 	for {
 		select {
 		case connection := <-manager.register:
 			manager.clients[connection] = true
-			fmt.Println("Added new connection!")
+			countClientsChannel1++
+			fmt.Println("Added new connection channel 1! clients: ", countClientsChannel1)
+			// fmt.Println("Added new connection!")
 		case connection := <-manager.unregister:
 			if _, ok := manager.clients[connection]; ok {
 				close(connection.data)
 				delete(manager.clients, connection)
-				fmt.Println("A connection has terminated!")
+				countClientsChannel1--
+				fmt.Println("A connection has terminated channel 1! clients: ", countClientsChannel1)
+				// fmt.Println("A connection has terminated!")
+			}
+		case message := <-manager.broadcast:
+			for connection := range manager.clients {
+				select {
+				case connection.data <- message:
+				default:
+					close(connection.data)
+					delete(manager.clients, connection)
+				}
+			}
+		}
+	}
+}
+
+func (manager *ClientManager) start2() {
+	for {
+		select {
+		case connection := <-manager.register:
+			manager.clients[connection] = true
+			countClientsChannel2++
+			fmt.Println("Added new connection channel 2! clients: ", countClientsChannel2)
+			// fmt.Println("Added new connection!")
+		case connection := <-manager.unregister:
+			if _, ok := manager.clients[connection]; ok {
+				close(connection.data)
+				delete(manager.clients, connection)
+				countClientsChannel2--
+				fmt.Println("A connection has terminated channel 2! clients: ", countClientsChannel2)
+				// fmt.Println("A connection has terminated!")
+			}
+		case message := <-manager.broadcast:
+			for connection := range manager.clients {
+				select {
+				case connection.data <- message:
+				default:
+					close(connection.data)
+					delete(manager.clients, connection)
+				}
+			}
+		}
+	}
+}
+
+func (manager *ClientManager) start3() {
+	for {
+		select {
+		case connection := <-manager.register:
+			manager.clients[connection] = true
+			countClientsChannel3++
+			fmt.Println("Added new connection channel 3! clients: ", countClientsChannel3)
+			// fmt.Println("Added new connection!")
+		case connection := <-manager.unregister:
+			if _, ok := manager.clients[connection]; ok {
+				close(connection.data)
+				delete(manager.clients, connection)
+				countClientsChannel3--
+				fmt.Println("A connection has terminated channel 3! clients: ", countClientsChannel3)
+				// fmt.Println("A connection has terminated!")
 			}
 		case message := <-manager.broadcast:
 			for connection := range manager.clients {
@@ -152,7 +218,7 @@ func channel_1() {
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
 	}
-	go manager.start()
+	go manager.start1()
 	for {
 		connection, _ := listener.Accept()
 		if error != nil {
@@ -180,7 +246,7 @@ func channel_2() {
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
 	}
-	go manager.start()
+	go manager.start2()
 	for {
 		connection, _ := listener.Accept()
 		if error != nil {
@@ -208,7 +274,7 @@ func channel_3() {
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
 	}
-	go manager.start()
+	go manager.start3()
 	for {
 		connection, _ := listener.Accept()
 		if error != nil {
